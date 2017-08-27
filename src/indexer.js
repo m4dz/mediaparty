17968ca395ac77b273c5f68771d8e3411c2adfdf
m4dz <code@m4dz.net>
2017-08-27 18:32:49 +0200

    [docs] add todo list for the indexer

--- src/indexer.js
+++ src/indexer.js
@@ -1,1 +1,10 @@
+'use strict'
+
 console.log( 'Hello, Background Indexer!' )
+
+// TODO: load Lunrjs to embed a search engine
+// TODO: fill Lunr index with Mediapart API contents
+// TODO: perform simple search on index to test the performance
+// TODO: use Storage API to store the last sync timestamp
+// TODO: use Alarm API to perform an index refresh every X minutes
+// TODO: use Storage API to stores a serialized index and rehydrate it at start
